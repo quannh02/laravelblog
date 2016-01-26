d244ee8c091cbc41a5dd1f5d4677642133184e02
quannh02 <quannguyen1456@gmail.com>
2016-01-26 09:31:42 +0700

    update database

--- chosach.sql
+++ chosach.sql
@@ -2,10 +2,10 @@
 -- version 4.4.14
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 05, 2016 at 06:16 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Client :  127.0.0.1
+-- Généré le :  Mar 26 Janvier 2016 à 03:30
+-- Version du serveur :  5.6.26
+-- Version de PHP :  5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `chosach`
+-- Base de données :  `chosach`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Structure de la table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `alias` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `order` int(11) NOT NULL,
+  `parent_id` int(10) unsigned NOT NULL,
+  `keywords` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `alias`, `order`, `parent_id`, `keywords`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'No Style', 'no-style', 1, 0, 'No Style', 'No Style', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Ao Thun No Style', 'ao-thun-no-style', 2, 1, 'Ao Thun No Style', 'Ao Thun No Style', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Ao Sơ Mi No Style', 'ao-so-mi-no-style', 0, 1, 'Áo Sơ Mi No Style', 'Áo Sơ Mi No Style', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'Adachi', 'adachi', 0, 0, 'Adachi', 'Adachi', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 'Áo thun Adachi', 'ao-thun-adachi', 0, 4, 'Áo thun Adachi', 'Ao thun Adachi', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migrations`
 --
 
 CREATE TABLE IF NOT EXISTS `migrations` (
@@ -32,17 +61,19 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `migrations`
+-- Contenu de la table `migrations`
 --
 
 INSERT INTO `migrations` (`migration`, `batch`) VALUES
 ('2014_10_12_000000_create_users_table', 1),
-('2014_10_12_100000_create_password_resets_table', 1);
+('2014_10_12_100000_create_password_resets_table', 1),
+('2016_01_23_162441_create_cates_table', 2),
+('2016_01_23_184931_create_product_table', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `password_resets`
+-- Structure de la table `password_resets`
 --
 
 CREATE TABLE IF NOT EXISTS `password_resets` (
@@ -54,12 +85,42 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `cate_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `price`, `description`, `cate_id`, `created_at`, `updated_at`) VALUES
+(1, 'Áo thun mùa đông', 50000, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 3, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Áo sơ mi cao cổ', 70000, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 3, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Áo ba lỗ', 130000, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 3, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'Áo đen', 500000, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 'Áo thun adachi 1', 54455, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'Áo thun adachi 1', 54455, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, 'Áo thun adachi 1', 54455, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, 'Áo thun adachi 1', 54455, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)', 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `fullname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `terms` tinyint(1) DEFAULT '0',
@@ -69,43 +130,64 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `terms`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'quan', 'quannh02@wru.vn', '$2y$10$zP9k.ZwGLsu9Iw4sROsnG.uqQtgty2E/z2hOsfUVi8vFuRH2pT/Vi', 0, '', '2016-01-05 16:41:22', '2016-01-01 00:10:04'),
-(4, 'quanpro', 'quannguyen@gmail.com', '$2y$10$oiwilXRUYjGcWTAPxHBnseUD7viCyWTvylLxh6OaXnKgPmGlniQeG', 0, '', '2016-01-05 16:41:23', '2016-01-01 02:26:58'),
+INSERT INTO `users` (`id`, `fullname`, `email`, `password`, `terms`, `remember_token`, `created_at`, `updated_at`) VALUES
+(4, 'quanpro1', 'quannguyen@gmail.com', '123123', 0, '', '2016-01-23 13:44:53', '2016-01-23 06:44:53'),
 (5, 'nguyenquan', 'quancute56@gmail.com', '$2y$10$VinPj02c2.vUpvDqAGgCuOVVv2j/0YZRM/LivCTIp1XGEmNQn5PFm', 0, '', '2016-01-05 16:41:24', '2016-01-01 23:27:33'),
 (6, 'quannh02', 'quannh04@gmail.com', '$2y$10$LFJYpwDwHtXn/HE63ZgaJe4ewstV7X7Y23Jl9i3mIbL783BNhhn1i', 0, '', '2016-01-05 16:41:25', '2016-01-04 20:33:40'),
 (7, 'quan32', 'quannh05@gmail.com', '$2y$10$ADfU25uY.02wz5mDNXXcTuqYQNYgrtg/DDmucZZNMyE6gmEzMQcJS', 0, '', '2016-01-05 16:41:25', '2016-01-04 20:37:10'),
 (8, 'quannh0234', 'quannh06@gmail.com', '$2y$10$ym5nZkjWxt9OxqlFR6ksn.KHoQrzuovqkUt3k/uTC/rR4JJfuvHZ.', 0, '', '2016-01-05 16:41:27', '2016-01-04 20:37:53'),
 (9, 'quan32', 'quannh07@gmail.com', '$2y$10$hHw1.Min2XFZ.6U2E/KEs.HVhO8bhjdZwESvN8f/Fi2dP5taBPpzG', 0, 'F609SZ2EYkLMFBnQgkycisumFvi7PI4PM6CVtAi0cbpLfU666NKzQasDPv28', '2016-01-05 17:14:13', '2016-01-05 10:14:13'),
-(10, 'quan', 'quannh123@gmail.com', '$2y$10$9ZpEHcB4MbiFDVWcGvPswuMBC9VgcjZAlroqzPToiq8OdNjrkit3y', 1, 'Ty1gag4qn1ldlKaW6oeRibETcfvUQdSJTLwvy3RRDKVjVvNotibbzpd47t68', '2016-01-05 17:10:15', '2016-01-05 10:10:15');
+(10, 'quan', 'quannh123@gmail.com', '$2y$10$9ZpEHcB4MbiFDVWcGvPswuMBC9VgcjZAlroqzPToiq8OdNjrkit3y', 1, 'Y5HFRRXQl87sQoMhJUGZFe5jeaGxHJrx1ElSQU3iRoTOuPBjcHEkjeE3GEe7', '2016-01-23 06:20:56', '2016-01-22 23:20:56');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `password_resets`
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`),
   ADD KEY `password_resets_token_index` (`token`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
