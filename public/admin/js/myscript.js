2bca61f56927122efc30d27ec43518de44f966cb
quannh02 <quannguyen1456@gmail.com>
2016-04-30 18:36:52 +0700

    cmommit1

--- public/admin/js/myscript.js
+++ public/admin/js/myscript.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-	var valueForCheckedCustomer_Type = $('#radioValueForCheck').val();
-	if(valueForCheckedCustomer_Type == 1){
-		document.getElementById("khachCT").checked = true;
-	} else {
-		document.getElementById("khachCN").checked = true;
-	}
+	// var valueForCheckedCustomer_Type = $('#radioValueForCheck').val();
+	// if(valueForCheckedCustomer_Type == 1){
+	// 	document.getElementById("khachCT").checked = true;
+	// } else {
+	// 	document.getElementById("khachCN").checked = true;
+	// }
+	$(".nav-pills li a").click(function(){
+		$('.nav-pills li').addClass('active');
+	});
 });
 $("div.alert").delay(3000).slideUp();
 
--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -36,7 +36,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav nav-tabs nav-pills">
               <li class=""><a href=""><i class="fa fa-bar-chart-o fa-fw"></i> Quản lý danh mục<span class="caret"></span></a></li>
-              <li class="active"><a href=""><i class="fa fa-cube fa-fw"></i></span>Quản lý xe<span class="caret"></span></a></li>
+              <li class=""><a href=""><i class="fa fa-cube fa-fw"></i>Quản lý xe<span class="caret"></span></a></li>
               <li class=""><a href=""><i class="fa fa-cube fa-fw"></i>Quản lý đơn đặt<span class="caret"></span></a></li>
               <li class=""><a href=""><i class="fa fa-users fa-fw"></i>Quản lý thành viên<span class="caret"></span></a></li>
               <li class="dropdown pull-right">
