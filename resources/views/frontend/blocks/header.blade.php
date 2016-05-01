550e42e4a56b2a5d7feedab4f550e67c2a99a16e
quannh02 <quannguyen1456@gmail.com>
2016-05-01 09:46:41 +0700

    cmommit4

--- resources/views/frontend/blocks/header.blade.php
+++ resources/views/frontend/blocks/header.blade.php
@@ -7,9 +7,9 @@
     </script>
     <script type="text/javascript" src="{{ url('public/user/js/votes.js') }}"></script>
     <div class="top margin-fixed-top">
-        <div class="col-xs-2" style="margin-left: 150px;">
-            <div class="container no-padding pos-res margin-site">
-                <a href="#"><img border="0" src="{{ url('public/user/images/logo.png') }}" class="logo img-responsive"></a>
+        <div class="col-xs-2">
+            <div class="">
+                <a href="#"><img src="{{ url('public/user/images/logo.png') }}" class="logo img-responsive" style="float:right;"></a>
             </div><!--end container-->
         </div>
         <div class="col-xs-4">
@@ -21,7 +21,7 @@
               <button type="submit" class="btn btn-default">Tìm kiếm</button>
             </form>
         </div>
-        <div class="col-xs-3">
+        <div class="col-xs-2">
             <div class="footer-social">
             <a href="" title="Facebook">
                 <img src="{{ url('public/user/images/facebook-icon.png') }}"></a>
@@ -31,7 +31,12 @@
                 <img src="{{ url('public/user/images/googleplus-icon.png') }}"></a>
             </div>
         </div>
-    
+        <div class="col-xs-3 linkthanhvien">
+            <ul>    
+                <li><a href="{{ url('auth/register') }}">Đăng ký</a></li>
+                <li><a href="{{ url('auth/login') }}">Đăng nhập</a></li>
+            </ul>
+        </div> 
 
     </div>
     
--- resources/views/frontend/pages/searchresult.blade.php
+++ resources/views/frontend/pages/searchresult.blade.php
@@ -1,26 +1,28 @@
 @extends('frontend.master')
 @section('description', 'Trang tìm kiếm')
 @section('content')
-<div class="container">
-    @if(isset($details))
-        <p> The Search results for your query <b> {{ $query }} </b> are :</p>
-    <h2>Sample User details</h2>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Hãng xe</th>
-                <th>Số chỗ</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($details as $user)
-            <tr>
-                <td>{{$user->hang_xe}}</td>
-                <td>{{$user->socho_xe}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @endif
+<div class="col-xs-7">
+	<div class="container">
+	    @if(isset($details))
+	        <p> Kết quả tìm kiếm cho <b> {{ $query }} </b> là :</p>
+	    <h2>Xe</h2>
+	    <table class="table table-striped">
+	        <thead>
+	            <tr>
+	                <th>Hãng xe</th>
+	                <th>Số chỗ</th>
+	            </tr>
+	        </thead>
+	        <tbody>
+	            @foreach($details as $user)
+	            <tr>
+	                <td>{{$user->hang_xe}}</td>
+	                <td>{{$user->socho_xe}}</td>
+	            </tr>
+	            @endforeach
+	        </tbody>
+	    </table>
+	    @endif
+	</div>
 </div>
 @endsection
